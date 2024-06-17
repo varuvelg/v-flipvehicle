e36cc778ccc7c7b1b5ffabdf9c21a85dd122fd70
varuvelg <154830789+varuvelg@users.noreply.github.com>
2024-06-17 22:03:12 +0300

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -2,15 +2,15 @@ Strings = {}
 
 Strings.Notify = {
     position = 'top-right', -- The position of all notifications
-    playerinvehicle = 'Sa ei saa sõidukit ümber pöörata kui oled ise sõidukis!',
-    novehiclenearby = 'Läheduses ei ole ühtegi sõidukit!',
-    vehiclealreadyflipped = 'Sõiduk on juba püsti!',
-    vehicleflipped = 'Sõiduk ümberpööratud!',
-    flippingcanceled = 'Ümberpööramine tühistatud!',
-    flippingfailed = 'Ümberpööramine ebaõnnestus!'
+    playerinvehicle = 'You cannot turn the vehicle around if you are in the vehicle yourself!',
+    novehiclenearby = 'There are no vehicles nearby!',
+    vehiclealreadyflipped = 'The vehicle is already up!',
+    vehicleflipped = 'Vehicle flipped!',
+    flippingcanceled = 'Flipping Canceled!',
+    flippingfailed = 'Flipping failed'
 }
 
 Strings.Progressbar = {
     position = 'bottom', -- The position of all progressbars
-    label = 'Sõiduki ümberpööramine'
+    label = 'Flipping vehicle'
 }
